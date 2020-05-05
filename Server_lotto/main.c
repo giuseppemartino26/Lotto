bc4cb23439a766a3602609fc37603b7d5f214693
Giuseppe <giuseppe.martino26@outlook.it>
2020-05-05 20:16:40 +0200

    !help and !signup done

--- Server_lotto/main.c
+++ Server_lotto/main.c
@@ -12,31 +12,6 @@
 #define _GNU_SOURCE
 
 
-/*int  accesso (const char* stringa)
-{
-        FILE* f1;
-        char* temp;
-        char* temp_word;
-        int len_word;
-        const char sp[2] = " ";
-        f1 = fopen("/home/giuseppe/Scrivania/utenti.txt","a+");
-
-        while (!feof(f1))
-        {
-            fgets(temp,N,f1);
-            temp_word = strtok(temp,sp);
-            len_word = strlen(temp_word);
-
-            if (strncmp(temp_word,stringa,len_word)== 0 )
-            {
-                fclose(f1);
-                return 1;
-            }
-        }
-        fclose(f1);
-        return 0;
-}
-*/
 struct users
 {
     char username[N];
@@ -44,15 +19,10 @@ struct users
     int session_id;
     FILE* fp;
 };
-//struct users users_list[N];
 
-/*void signup_fun(char string[], char* tab[])
-{
-
-} */
 
 int main(int argc, char* argv[]) {
-    printf("Ci sei?\n");
+  //  printf("Ci sei?\n");
     int sd, new_sd, ret, len, len_msg_signup;
     struct sockaddr_in my_addr;
     struct sockaddr_in cl_addr;
@@ -65,9 +35,7 @@ int main(int argc, char* argv[]) {
     const char s[2] = " ";
     struct users users_list[N];
     int i;
-    int ciao = 0;
     int cont = 0;
-    int len_list = 0; //lunghezza lista di utenti registrati
     char msg_signup[50];
     FILE* f1;
     char* temp;
@@ -120,15 +88,17 @@ int main(int argc, char* argv[]) {
             while (1) {
                 printf("pronto per ricevere\n");
                 fflush(stdout);
+
                 // Attendo dimensione del mesaggio
                 ret = recv(new_sd, (void *) &lmsg, sizeof(uint16_t), 0);
                 printf("ok");
                 fflush(stdout);
 
-                len = ntohs(lmsg); // Rinconverto in formato host
+                // Rinconverto in formato host
+                len = ntohs(lmsg);
 
-                ret = recv(new_sd, (void *) buffer, len,
-                           0); // ora che so la lunghezza (len), ricevo il messaggio effettivo di lunghezza len
+                // ricevo il messaggio effettivo di lunghezza len
+                ret = recv(new_sd, (void *) buffer, len,0);
                 printf("Buffer ricevuto");
 
                 if (ret < 0) {
@@ -150,19 +120,10 @@ int main(int argc, char* argv[]) {
 
                     f1 = fopen("/home/giuseppe/Scrivania/utenti.txt","a+");
 
-                    //while (!feof(f1))
-                  //  for (int j = 1; j != feof(f1) ; j++)
-                   // for (int j = 0; j <= 10; j++)
-                  //  while (fgets(line, line_size, f1) != 0)
 
-                 //   temp_word = strtok(lline,s);
-                  //  printf("%s\n", temp_word);
                     flag = 0;
                     while ((read = getline(&lline, &length, f1)) != -1)
-                  {
-                       // ustmp = strtok(lline,s);
-                       // ustmp = strtok(NULL,s);
-                      //  printf("ustmp è %s", ustmp);
+                    {
 
                       if (strncmp(strtok(lline,s),us,strlen(us)) == 0 )
                       {
@@ -176,45 +137,11 @@ int main(int argc, char* argv[]) {
                           lmsg_signup = htons(len_msg_signup);
                           ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
                           ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-
-
-
-
                           break;
                       }
-                       /* fgets(temp,N,f1);
-                        temp_word = strtok(line,s);
-                      //  len_word = strlen(temp_word);
-
-                        if (strcmp(temp_word,us) == 0 )
-                        {
-                            strcpy(msg_signup, "Username non disponibile, riprovare\n");
-                            len_msg_signup = strlen(msg_signup) + 1;
-                            lmsg_signup = htons(len_msg_signup);
-                            ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
-                            ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-                           // fclose(f1);
-                          //  continue;
-                        } else{
-                            strcpy(msg_signup, "Registazione avvenuta con successo\n");
-                            len_msg_signup = strlen(msg_signup) + 1;
-                            lmsg_signup = htons(len_msg_signup);
-                            ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
-                            ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-                            printf("finito");
-                            fflush(stdout);
-                           // fprintf(f1,"%s",buffer);
-                            fprintf(f1,"%s",us);
-                         //   flag = 1;
-                          //  fclose(f1);
-                        }
-                        */
-                    /*  printf("%s\n", lline);
-                      fflush(stdout);*/
-
-
                     }
-                    printf("%d\n",flag);
+
+                    //printf("%d\n",flag);
 
                     if (flag == 0)
                     {
@@ -231,10 +158,6 @@ int main(int argc, char* argv[]) {
                         lmsg_signup = htons(len_msg_signup);
                         ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
                         ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-
-
-
-
                     }
 
 
@@ -242,59 +165,6 @@ int main(int argc, char* argv[]) {
                     if (lline) {
                         free(lline);
                     }
-
-
-
-
-                   // if (accesso(us) == 1)
-                  // ciao = accesso(us);
-                    //    printf("%d", ciao);
-                      /*
-                    {
-                        strcpy(msg_signup, "Username non disponibile, riprovare\n");
-                        len_msg_signup = strlen(msg_signup) + 1;
-                        lmsg_signup = htons(len_msg_signup);
-                        ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
-                        ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-                    } else{
-                        strcpy(msg_signup, "Registazione avvenuta con successo\n");
-                        len_msg_signup = strlen(msg_signup) + 1;
-                        lmsg_signup = htons(len_msg_signup);
-                        ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
-                        ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-
-                    }
-                 /*   pwd = strtok(NULL, s);
-
-                    cont=0;
-                    printf("Cont prima del for: %d\n",cont);
-
-                    for (i = 0; i < len_list; i++) {
-                        if (strncmp(us, users_list[i].username, strlen(us)) == 0) {
-                            cont++;
-                        }
-                    }
-
-                    printf("Cont dopo del for: %d\n",cont);
-
-                    if (cont > 0) {
-                        strcpy(msg_signup, "Username non disponibile, riprovare\n");
-                        len_msg_signup = strlen(msg_signup) + 1;
-                        lmsg_signup = htons(len_msg_signup);
-                        ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
-                        ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-                    } else {
-                        strcpy(users_list[i].username, us);
-                        strcpy(users_list[i].password, pwd);
-                        len_list++;
-                        strcpy(msg_signup, "Registazione avvenuta con successo\n");
-                        len_msg_signup = strlen(msg_signup) + 1;
-                        lmsg_signup = htons(len_msg_signup);
-                        ret = send(new_sd, (void *) &lmsg_signup, sizeof(uint16_t), 0);
-                        ret = send(new_sd, (void *) msg_signup, len_msg_signup, 0);
-                    }
-*/
-
                 }
 
             }
